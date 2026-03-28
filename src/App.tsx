87615f0787dcee7f0d9fe9731469211be552467f
Saqib <syedsaqibali0786@gmail.com>
2026-03-28 06:21:33 +0530

    initial commit (react + tailwind )

---
+++ src/App.tsx
@@ -0,0 +1,11 @@
+function App() {
+  return (
+    <div className="flex items-center justify-center h-screen bg-black">
+      <h1 className="text-4xl text-green-400 font-bold">
+        Tailwind Working 🚀
+      </h1>
+    </div>
+  )
+}
+
+export default App
