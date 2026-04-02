3da784c5e1e7658b3017ecf6698be71297dbc00b
Saqib <syedsaqibali0786@gmail.com>
2026-04-03 04:54:57 +0530

    Navbar complete

--- src/component/Navbar/Navbar.tsx
+++ src/component/Navbar/Navbar.tsx
@@ -1,7 +1,10 @@
 import { useEffect, useEffectEvent, useRef, useState } from "react";
+import { TfiAlignCenter } from "react-icons/tfi";
+import { RxCross1 } from "react-icons/rx";
 import Logo from "../../assets/Logo_S.png";
 import "./navbar.css";
 import { useLocation } from "react-router";
+import Slide from "../slider/Slide";
 
 type StyleType = {
   left: number;
@@ -9,16 +12,25 @@ type StyleType = {
 };
 
 const Navbar = () => {
-  const location   = useLocation();
-  const [style, setStyle] = useState<StyleType>({ left: 0, width: 0 });
-  const [active, setActive] = useState<string>(location.hash.replace("#", "") || "About" );
+  const location = useLocation();
+  const [Swidth, setSwidth] = useState<number>(window.innerWidth);
+  const [openclose, setOpenclose] = useState<boolean>(false);
 
+  useEffect(() => {
+    window.onresize = () => setSwidth(window.innerWidth);
+  }, []);
+
+  const [hovered, setHovered] = useState(false);
+  const [style, setStyle] = useState<StyleType>({ left: 0, width: 0 });
+  const [active, setActive] = useState<string>(
+    location.hash.replace("#", "") || "About",
+  );
 
   const listRef = useRef<HTMLUListElement | null>(null);
 
   const moveIndicator = (el: HTMLElement) => {
     setStyle({
-      left: el.offsetLeft ,
+      left: el.offsetLeft,
       width: el.offsetWidth,
     });
   };
@@ -47,69 +59,109 @@ const Navbar = () => {
   ];
 
   return (
-    <div className="navbar_menu flex justify-center ">
-      <div className="max-[1350px]:w-[85%] max-[1150px]:w-[90%] max-[850px]:w-[95%] w-[80%] mt-[10px] bg-[#14142a] max-[1150px]:py-[3px] py-[5px] max-[1150px]:px-[15px] px-[20px] rounded-3xl flex justify-between items-center">
-        <div>
-          <img src={Logo} alt="" width="100%" className="w-[80px] max-[1650px]:w-[70px] max-[1350px]:w-[60px] max-[1150px]:w-[50px]" />
-        </div>
+    <>
+      <div className="navbar_menu flex justify-center ">
+        <div className="max-[1350px]:w-[85%] max-[1150px]:w-[90%] max-[850px]:w-[95%] w-[80%] mt-[10px] bg-[#14142a] max-[1150px]:py-[3px] py-[5px] max-[1150px]:px-[15px] px-[20px] rounded-3xl flex justify-between items-center">
+          <div>
+            <img
+              src={Logo}
+              alt=""
+              width="100%"
+              className="w-[80px] max-[1650px]:w-[70px] max-[1350px]:w-[60px] max-[1150px]:w-[50px]"
+            />
+          </div>
+          {Swidth > 720 && (
+            <div className="menu">
+              <span
+                className="indicator"
+                style={{
+                  left: style.left,
+                  width: style.width,
+                  opacity: hovered || active ? 1 : 0,
+                }}
+              />
 
-        <div className="menu">
-          <span
-            className="indicator"
-            style={{
-              left: style.left,
-              width: style.width,
-            }}
-          />
+              <ul
+                ref={listRef}
+                className="flex"
+                onMouseLeave={() => {
+                  setHovered(false);
 
-          <ul
-            ref={listRef}
-            className="flex"
-            onMouseLeave={() => {
-              const index = menuItems.indexOf(active);
-              const el = listRef.current?.children[index]?.querySelector(
-                "a",
-              ) as HTMLElement | null;
+                  if (!active) return;
 
-              if (el) moveIndicator(el);
-            }}
-          >
-            {menuItems.map((item) => (
-              <li key={item} >
+                  const index = menuItems.indexOf(active);
+                  const el = listRef.current?.children[index]?.querySelector(
+                    "a",
+                  ) as HTMLElement | null;
+
+                  if (el) moveIndicator(el);
+                }}
+              >
+                {menuItems.map((item) => (
+                  <li key={item}>
+                    <a
+                      href={`#${item}`}
+                      onClick={() => setActive(item)}
+                      onMouseEnter={(e) => {
+                        setHovered(true);
+                        moveIndicator(e.currentTarget as HTMLElement);
+                      }}
+                      className={`${active === item ? "active" : ""}`}
+                    >
+                      {item}
+                    </a>
+                  </li>
+                ))}
+              </ul>
+            </div>
+          )}
+
+          <div className="let_but flex items-center">
+            <div className="let_but_inner mt-[5px]">
+              <p className="">
                 <a
-                  href={`#${item}`}
-                  onClick={() => setActive(item)}
-                  onMouseEnter={(e) => {
-                    moveIndicator(e.currentTarget as HTMLElement);
-                  }}
-                  className={`${
-                    active === item ? "active" : ""
+                  href="#letstalk"
+                  onClick={() => setActive("letstalk")}
+                  className={`py-[10px] px-[35px] rounded-2xl border-[2px] ${
+                    active === "letstalk" ? "active_letstalk" : ""
                   }`}
                 >
-                  {item}
+                  <span className="m-[0px] p-[0px]">Let's talk</span>
                 </a>
-              </li>
-            ))}
-          </ul>
-        </div>
-
-        <div className="let_but">
-          <div className="let_but_inner mt-[5px]">
-            <p className="">
-              <a
-                href="#letstalk"
-                onClick={() => setActive("letstalk")}
-                className={`py-[10px] px-[35px] rounded-2xl border-[2px] ${
-                  active === "letstalk" ? "active_letstalk" : ""
-                }`}
-              >
-                <span className="m-[0px] p-[0px]">Let's talk</span>
-              </a>
-            </p>
+              </p>
+            </div>
+            {Swidth < 720 && (
+              <div>
+                <div className="ml-[10px] relative z-50">
+                  {openclose ? (
+                    <RxCross1
+                      color="#8b5cf6"
+                      size="20px"
+                      cursor="pointer"
+                      className="icon"
+                      onClick={() => {
+                        setOpenclose(!openclose);
+                      }}
+                    />
+                  ) : (
+                    <TfiAlignCenter
+                      color="#8b5cf6"
+                      size="20px"
+                      cursor="pointer"
+                      className="icon"
+                      onClick={() => {
+                        setOpenclose(!openclose);
+                      }}
+                    />
+                  )}
+                </div>
+              </div>
+            )}
           </div>
         </div>
       </div>
-    </div>
+      <div> <Slide openclose={openclose} setOpenclose={setOpenclose}/></div>
+    </>
   );
 };
 
---
+++ src/component/slider/Slide.tsx
@@ -0,0 +1,73 @@
+import { useEffect } from "react";
+import "./slide.css";
+type SlideProps = {
+  openclose: boolean;
+  setOpenclose: React.Dispatch<React.SetStateAction<boolean>>;
+};
+
+const Slide = ({ openclose, setOpenclose }: SlideProps) => {
+    console.log(document.querySelector('.sidebar'))
+  useEffect(() => {
+    if (openclose) {
+      document.body.style.overflow = "hidden";
+    } else {
+      document.body.style.overflow = "auto";
+    }
+
+    return () => {
+      document.body.style.overflow = "auto";
+    };
+  }, [openclose]);
+
+  return (
+    <div className={`slide_container ${openclose ? "open" : ""}`}>
+      <div className="absolute inset-0" onClick={() => setOpenclose(false)} />
+      {/* <div
+        className={`slide_container flex w-full h-full z-40 bg-[#08081085] absolute top-0 bottom-0 ${
+          openclose ? "open" : ""
+        }`}
+      > */}
+      <div className="flex w-full h-full">
+        <div className="max-[500]:w-[20%] w-[30%]" />
+        <div className="sidebar max-[500]:w-[80%] w-[70%] h-full bg-[#14142a]">
+          <div className="Menu_list_slide pt-[15vh]">
+            <ul className="max-[500]:pl-[10%] pl-[15%]">
+              <li className="mb-[10px]">
+                <a href="#About" onClick={() => setOpenclose(false)}>
+                  About
+                </a>
+              </li>
+              <li className="mb-[10px]">
+                <a href="#Skills" onClick={() => setOpenclose(false)}>
+                  Skills
+                </a>
+              </li>
+              <li className="mb-[10px]">
+                <a href="#Experience" onClick={() => setOpenclose(false)}>
+                  Experience
+                </a>
+              </li>
+              <li className="mb-[10px]">
+                <a href="#Work" onClick={() => setOpenclose(false)}>
+                  Work
+                </a>
+              </li>
+              <li className="mb-[10px]">
+                <a href="#Contact" onClick={() => setOpenclose(false)}>
+                  Contact
+                </a>
+              </li>
+              <li className="mb-[10px]">
+                <a href="#Blog" onClick={() => setOpenclose(false)}>
+                  Blog
+                </a>
+              </li>
+            </ul>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default Slide;
