0e2ccb851d44f8426779370b3363fbbe8c21cf05
Saqib <syedsaqibali0786@gmail.com>
2026-04-02 20:53:13 +0530

    navbar update

--- src/component/Navbar/Navbar.tsx
+++ src/component/Navbar/Navbar.tsx
@@ -1,89 +1,109 @@
-import { useState } from "react";
+import { useEffect, useEffectEvent, useRef, useState } from "react";
 import Logo from "../../assets/Logo_S.png";
 import "./navbar.css";
+import { useLocation } from "react-router";
 
-const Navbar: React.FC = () => {
-  const [active, setActive] = useState<string>("");
+type StyleType = {
+  left: number;
+  width: number;
+};
 
-  const handleClick = (id: string) => {
-    setActive(id);
+const Navbar = () => {
+  const location   = useLocation();
+  const [style, setStyle] = useState<StyleType>({ left: 0, width: 0 });
+  const [active, setActive] = useState<string>(location.hash.replace("#", "") || "About" );
+
+
+  const listRef = useRef<HTMLUListElement | null>(null);
+
+  const moveIndicator = (el: HTMLElement) => {
+    setStyle({
+      left: el.offsetLeft ,
+      width: el.offsetWidth,
+    });
   };
+
+  const updateIndicator = useEffectEvent(() => {
+    const index = menuItems.indexOf(active);
+
+    const el = listRef.current?.children[index]?.querySelector(
+      "a",
+    ) as HTMLElement | null;
+
+    if (el) moveIndicator(el);
+  });
+
+  useEffect(() => {
+    updateIndicator();
+  }, []);
+
+  const menuItems: string[] = [
+    "About",
+    "Skills",
+    "Experience",
+    "Work",
+    "Contact",
+    "Blog",
+  ];
 
   return (
     <div className="navbar_menu flex justify-center ">
-      <div className="w-[80%] m-[10px] bg-[#14142a] py-[5px] px-[20px] rounded-3xl flex justify-between items-center">
+      <div className="max-[1350px]:w-[85%] max-[1150px]:w-[90%] max-[850px]:w-[95%] w-[80%] mt-[10px] bg-[#14142a] max-[1150px]:py-[3px] py-[5px] max-[1150px]:px-[15px] px-[20px] rounded-3xl flex justify-between items-center">
         <div>
-          <img src={Logo} alt="" width="100%" className="w-[80px]" />
+          <img src={Logo} alt="" width="100%" className="w-[80px] max-[1650px]:w-[70px] max-[1350px]:w-[60px] max-[1150px]:w-[50px]" />
         </div>
+
         <div className="menu">
-          <ul className="flex">
-            <li>
-              <a
-                href="#About"
-                onClick={() => handleClick("About")}
-                className={active === "About" ? "active" : ""}
-              >
-                About
-              </a>
-            </li>
-            <li>
-              <a
-                href="#Skills"
-                onClick={() => handleClick("Skills")}
-                className={active === "Skills" ? "active" : ""}
-              >
-                Skills
-              </a>
-            </li>
-            <li>
-              <a
-                href="#Experience"
-                onClick={() => handleClick("Experience")}
-                className={active === "Experience" ? "active" : ""}
-              >
-                Experience
-              </a>
-            </li>
-            <li>
-              <a
-                href="#Work"
-                onClick={() => handleClick("Work")}
-                className={active === "Work" ? "active" : ""}
-              >
-                Work
-              </a>
-            </li>
-            <li>
-              <a
-                href="#Contact"
-                onClick={() => handleClick("Contact")}
-                className={active === "Contact" ? "active" : ""}
-              >
-                Contact
-              </a>
-            </li>
-            <li>
-              <a
-                href="#Blog"
-                onClick={() => handleClick("Blog")}
-                className={active === "Blog" ? "active" : ""}
-              >
-                Blog
-              </a>
-            </li>
+          <span
+            className="indicator"
+            style={{
+              left: style.left,
+              width: style.width,
+            }}
+          />
+
+          <ul
+            ref={listRef}
+            className="flex"
+            onMouseLeave={() => {
+              const index = menuItems.indexOf(active);
+              const el = listRef.current?.children[index]?.querySelector(
+                "a",
+              ) as HTMLElement | null;
+
+              if (el) moveIndicator(el);
+            }}
+          >
+            {menuItems.map((item) => (
+              <li key={item} >
+                <a
+                  href={`#${item}`}
+                  onClick={() => setActive(item)}
+                  onMouseEnter={(e) => {
+                    moveIndicator(e.currentTarget as HTMLElement);
+                  }}
+                  className={`${
+                    active === item ? "active" : ""
+                  }`}
+                >
+                  {item}
+                </a>
+              </li>
+            ))}
           </ul>
         </div>
+
         <div className="let_but">
-          <div className="let_but_inner">
-            <p>
+          <div className="let_but_inner mt-[5px]">
+            <p className="">
               <a
                 href="#letstalk"
-                onClick={() => handleClick("letstalk")}
-                className={`py-[10px] px-[35px] rounded-2xl border-[2px] shadow-[0_5px_15px_rgba(99,179,237,0.4)] ${
+                onClick={() => setActive("letstalk")}
+                className={`py-[10px] px-[35px] rounded-2xl border-[2px] ${
                   active === "letstalk" ? "active_letstalk" : ""
                 }`}
               >
-                <span>Let's talk</span>
+                <span className="m-[0px] p-[0px]">Let's talk</span>
               </a>
             </p>
           </div>
@@ -94,3 +114,13 @@ const Navbar: React.FC = () => {
 };
 
 export default Navbar;
+
+// ----------------------------
+// useEffect(() => {
+//   const index = menuItems.indexOf(active);
+//   const el = listRef.current?.children[index]?.querySelector(
+//     "a",
+//   ) as HTMLElement | null;
+
+//   if (el) moveIndicator(el);
+// }, [active]);
